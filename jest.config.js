1b4d905e880f81101bc7d06565a49c6be144b218
shippokun <okashi0tabetai@gmail.com>
2020-12-15 00:54:09 +0900

    chore(*): adjust testing config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel'
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'vue'],
+  setupFiles: ['./test-setup.js'],
+  testMatch: ['**/*.spec.{j,t}s?(x)'],
+  collectCoverage: true
 };
---
+++ test-setup.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import VueCompositionApi from '@vue/composition-api';
+
+Vue.use(Vuetify);
+Vue.use(VueCompositionApi);
+Vue.config.productionTip = false;
