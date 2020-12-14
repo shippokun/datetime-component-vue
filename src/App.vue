6fc92771dcdf9bebebe56c289046d30e8af4796b
shippokun <okashi0tabetai@gmail.com>
2020-12-15 01:00:26 +0900

    chore(App): include datetime component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,34 @@
 <template>
-  <div id="app"></div>
+  <v-app>
+    <v-container>
+      <v-row justify="center">
+        <h1>Datetime Component</h1>
+      </v-row>
+      <v-row justify="center">
+        <v-col cols="6">
+          <datetime-component v-model="datetime"></datetime-component>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
+import { defineComponent, ref } from '@vue/composition-api';
+
+import { DatetimeComponent } from '@/components';
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+  components: {
+    DatetimeComponent
+  },
+  setup() {
+    const datetime = ref('');
+
+    return {
+      datetime
+    };
+  }
 });
 </script>
